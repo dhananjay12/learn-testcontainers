a019e8b771808f6fc1e98a9b3b95136b4ca2e00a
dhananjay12 <dhananjay1204@gmail.com>
2020-10-08 23:09:58 +0200

    crud service

---
+++ crud-service/src/test/java/com/djcodes/test/containers/crudservice/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djcodes.test.containers.crudservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
